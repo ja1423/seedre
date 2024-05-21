e774ec43add18d05e2028f28861dd5aaebd2f261
ja1423 <aziabdujabborova1423@gmail.com>
2024-05-21 08:41:39 +0500

    seedre

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "20px",
+    },
+    colors: {
+      primary: "#13ce66",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
